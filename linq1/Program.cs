934ea9dafee6de611388538f6cf641c12a607db5
habibasakr2006 <habiba.sakr09@gmail.com>
2026-09-10 21:33:44 +0300

    Question 3

--- linq1/Program.cs
+++ linq1/Program.cs
@@ -172,12 +172,12 @@ namespace linq1
             #endregion
 
             #region Question 3
-            var q3 = ProductList.OrderBy(p => p.UnitPrice);
+            //var q3 = ProductList.OrderBy(p => p.UnitPrice);
 
-            foreach (var item in q3)
-            {
-                Console.WriteLine($"Name: {item.ProductName}, Price: {item.UnitPrice}");
-            }
+            //foreach (var item in q3)
+            //{
+            //    Console.WriteLine($"Name: {item.ProductName}, Price: {item.UnitPrice}");
+            //}
             #endregion
 
             #region Question 4
